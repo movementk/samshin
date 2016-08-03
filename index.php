13e047f06d6dc956c21d29a6017c3ab0ef0b3d3b
Kim Jaehong <mk.kimjh@gmail.com>
2016-08-03 20:42:44 +0900

    레이아웃 작성 중...

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>삼신다이아몬드</title>
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="/assets/css/default.css" rel="stylesheet">
+</head>
+<body>
+    <header id="header">
+        <div class="util">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        회원메뉴
+                    </div>
+                    <div class="col-lg-offset-4 col-lg-4">
+                        검색영역
+                    </div>
+                </div>
+            </div>
+        </div>
+        <h1 class="logo">DiaOUTLET BY SAMSHIN</h1>
+        <nav id="gnb" tabindex="-1">
+        </nav>
+    </header>
+    <main id="content" tabindex="-1">
+    </main>
+    <nav id="">
+    </nav>
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="company">
+                        <ul class="policy">
+                            <li><a href="#">회사소개</a></li>
+                            <li><a href="#">이용약관</a></li>
+                            <li><a href="#">개인정보처리방침</a></li>
+                        </ul>
+                        <dl class="company">
+                            <dt>상호</dt>
+                            <dd>(주)삼신다이아몬드</dd>
+                            <dt>대표</dt>
+                            <dd>신명숙</dd>
+                        </dl>
+                        <dl class="address">
+                            <dt>주소</dt>
+                            <dd><adress>06025 서울특별시 강남구 논현로 836 삼신빌딩 삼신다이아몬드 인터넷 사업부</adress></dd>
+                        </dl>
+                        <dl class="license">
+                            <dt>통신판매신고번호</dt>
+                            <dd>제 2001-서울강남-04672호</dd>
+                            <dt>사업자번호</dt>
+                            <dd>211-86-01694  [<a href="#" target="_blank">사업자정보확인</a>]</dd>
+                        </dl>
+                        <dl class="contact">
+                            <dt>전화</dt>
+                            <dd>02-512-8484</dd>
+                            <dt>팩스</dt>
+                            <dd>02-546-7342</dd>
+                            <dt>개인정보보호책임자</dt>
+                            <dd>김찬훈</dd>
+                            <dt>이메일</dt>
+                            <dd><a href="mailto:admin@samshin.co.kr" target="_blank">admin@samshin.co.kr</a></dd>
+                        </dl>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <h2 class="visible-lg"><img src="/assets/images/logo_grey.png" alt="DiaOUTLET BY SAMSHIN"></h2>
+                    <p class="copyright">
+                        © 1988-2016 DIAOUTLET.  ALL RIGHTS RESERVED.<br>
+                        <span>SITE BY <a href="http://www.movementk.com" target="_blank">MOVEMENT K GROUP<span></a>.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
