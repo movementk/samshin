cc0105e1aea0e58b75eea0ab1020900c0430d114
Kim Jaehong <mk.kimjh@gmail.com>
2016-08-04 20:04:25 +0900

    레이아웃 작성 중...

--- index.php
+++ index.php
@@ -32,7 +32,81 @@
     </header>
     <main id="content" tabindex="-1">
     </main>
-    <nav id="">
+    <nav id="nav-foot">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-lg-8 visible-lg">
+                    <div class="full">
+                        <ul class="row">
+                            <li class="col-lg-3">
+                                <h5>COMPANY</h5>
+                                <ul>
+                                    <li><a href="#">CEO 인사말</a></li>
+                                    <li><a href="#">연혁</a></li>
+                                    <li><a href="#">비전 &amp; 미션</a></li>
+                                    <li><a href="#">매장 예약 안내</a></li>
+                                    <li><a href="#">오시는길</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-lg-3">
+                                <h5>PRODUCT</h5>
+                                <ul>
+                                    <li><a href="#">다이아몬드</a></li>
+                                    <li><a href="#">다이아몬드 반지</a></li>
+                                    <li><a href="#">다이아몬드 목걸이</a></li>
+                                    <li><a href="#">다이아몬드 귀걸이</a></li>
+                                    <li><a href="#">완제품</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-lg-3">
+                                <h5>GUIDE</h5>
+                                <ul>
+                                    <li><a href="#">다이아몬드 정보(4C)</a></li>
+                                    <li><a href="#">다이아몬드 쉐입(형태) 정보</a></li>
+                                    <li><a href="#">펜시 다이아몬드 정보</a></li>
+                                    <li><a href="#">진주 정보</a></li>
+                                    <li><a href="#">금속 정보</a></li>
+                                    <li><a href="#">반지사이즈 측정법</a></li>
+                                    <li><a href="#">감정서 안내</a></li>
+                                    <li><a href="#">제작자 안내(금옥/명장)</a></li>
+                                    <li><a href="#">디자이너 소개</a></li>
+                                </ul>
+                            </li>
+                            <li class="col-lg-3">
+                                <h5>CS CENTER</h5>
+                                <ul>
+                                    <li><a href="#">다이아몬드 제품 구매 방법</a></li>
+                                    <li><a href="#">제작 및 배송 안내</a></li>
+                                    <li><a href="#">주문/배송조회</a></li>
+                                    <li><a href="#">공지사항</a></li>
+                                    <li><a href="#">구매후기</a></li>
+                                    <li><a href="#">FAQ</a></li>
+                                    <li><a href="#">1:1 문의하기</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-lg-3 col-lg-offset-1">
+                    <div class="cs">
+                        <h5>고객상담센터</h5>
+                        <p class="contact"><strong>02-512-8484</strong></p>
+                        <dl class="hours">
+                            <dt>평일</dt>
+                            <dd>09:00 ~ 19:00</dd>
+                            <dt>점심시간</dt>
+                            <dd>12:00 ~ 13:00</dd>
+                        </dl>
+                        <p class="note">※ 주말 및 공휴일은 휴무입니다.</p>
+                        <ul class="row">
+                            <li class="col-xs-4 col-sm-2 col-sm-offset-3 col-lg-4 col-lg-offset-0"><a class="btn btn-block" href="#">방문예약신청</a></li>
+                            <li class="col-xs-4 col-sm-2 col-lg-4"><a class="btn btn-block" href="#">1:1문의</a></li>
+                            <li class="col-xs-4 col-sm-2 col-lg-4"><a class="btn btn-block" href="#">배송조회</a></li>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </nav>
     <footer id="footer">
         <div class="container">
@@ -58,7 +132,7 @@
                             <dt>통신판매신고번호</dt>
                             <dd>제 2001-서울강남-04672호</dd>
                             <dt>사업자번호</dt>
-                            <dd>211-86-01694  [<a href="#" target="_blank">사업자정보확인</a>]</dd>
+                            <dd>211-86-01694  <a href="#" target="_blank">[사업자정보확인]</a></dd>
                         </dl>
                         <dl class="contact">
                             <dt>전화</dt>
