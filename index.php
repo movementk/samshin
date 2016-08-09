85ec2d57e21ef50ca1c5b97e91744d4ecc8d65e9
mk-kimjh <mk-kimjh@gmail.com>
2016-08-09 18:41:24 +0900

    레이아웃 작성 중...

--- index.php
+++ index.php
@@ -6,31 +6,80 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>삼신다이아몬드</title>
     <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/css/default.css" rel="stylesheet">
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="/assets/css/default.css" rel="stylesheet">
 </head>
 <body>
+    <a href="#gnb" class="skip-nav sr-only sr-only-focusable">주메뉴로 건너뛰기</a>
+    <a href="#content" class="skip-nav sr-only sr-only-focusable">컨텐츠로 건너뛰기</a>
     <header id="header">
-        <div class="util">
+        <div class="util visible-lg">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-4">
-                        회원메뉴
+                        <ul class="member">
+                            <li class="login"><a href="#">LOGIN</a></li>
+                            <li class="mypage"><a href="#">MYPAGE</a></li>
+                            <li class="cart"><a href="#">CART(<strong>0</strong>)</a></li>
+                        </ul>
                     </div>
                     <div class="col-lg-offset-4 col-lg-4">
-                        검색영역
+                        <form class="search" action="#">
+                            <div class="form-group">
+                                <label for="search-words" class="sr-only">검색어</label>
+                                <input type="text" class="form-control" id="search-words" placeholder="검색어를 입력해 주세요">
+                            </div>
+                            <button type="submit" class="btn btn-search">
+                                <span class="glyphicon glyphicon-search"></span>
+                                <span class="sr-only">검색</span>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-        <h1 class="logo">DiaOUTLET BY SAMSHIN</h1>
+        <div class="logo">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-2 hidden-lg">
+                        <button type="button" class="btn btn-menu" aria-hidden="true">
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="bar"></span>
+                            <span class="sr-only">메뉴열기</span>
+                        </button>
+                    </div>
+                    <div class="col-xs-8 col-lg-12">
+                        <h1>
+                            <a href="#"><img src="/assets/images/logo.png" alt="DiaOUTLET BY SAMSHIN"></a>
+                        </h1>
+                    </div>
+                    <div class="col-xs-2 hidden-lg">
+                        <button type="button" class="btn btn-search" aria-hidden="true">
+                            <span class="glyphicon glyphicon-search"></span>
+                            <span class="sr-only">검색</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <nav id="gnb" tabindex="-1">
+            <div class="container">
+                <ul>
+                    <li><a href="#">다이아몬드 검색</a></li>
+                    <li><a href="#">반디 디자인 찾기</a></li>
+                    <li><a href="#">목걸이 디자인 찾기</a></li>
+                    <li><a href="#">귀걸이 디자인 찾기</a></li>
+                    <li><a href="#">완제품/당일발송</a></li>
+                    <li><a href="#">보석정보</a></li>
+                </ul>
+            </div>
         </nav>
     </header>
-    <main id="content" tabindex="-1">
+    <main id="content" tabindex="-1" style="min-height: 500px;">
     </main>
     <nav id="nav-foot">
         <div class="container">
